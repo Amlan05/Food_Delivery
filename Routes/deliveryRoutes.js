e3d768d1215eb684c0f0a6a74fa3b2d7590b787f
Amlan05 <itzamlan05@gmail.com>
2024-04-01 12:23:35 +0530

    okupdt

--- Routes/deliveryRoutes.js
+++ Routes/deliveryRoutes.js
@@ -18,15 +18,19 @@ const deliveryController = require('../Controllers/deliveryController');
  *               zone:
  *                 type: string
  *                 description: The delivery zone.
+ *                 example: central
  *               organization_id:
  *                 type: integer
  *                 description: The organization ID.
+ *                 example: 1
  *               total_distance:
  *                 type: number
  *                 description: The total distance of the delivery.
+ *                 example: 10
  *               item_id:
- *                 type: string
+ *                 type: integer   # Corrected to integer
  *                 description: The item ID.
+ *                 example: 1
  *     responses:
  *       200:
  *         description: Successful operation
